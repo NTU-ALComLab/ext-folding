887857850fc6e276436324c7ffed2c0a4855ca54
b04901112 <b04901112@ntu.edu.tw>
2019-10-03 16:07:15 +0800

    reord done (untested)

--- timeMux2/reord.cpp
+++ timeMux2/reord.cpp
@@ -45,17 +45,17 @@ static void initSupVecs(Abc_Ntk_t *pNtk, DdManager *dd, DdNode **nodeVec, SupVec
 class SupVecCompFunc
 {
   public:
-    SupVecCompFunc(SupVecs& svs): sVecs(svs) {}
+    SupVecCompFunc(const SupVecs& svs): sVecs(svs) {}
     bool operator() (cuint x, cuint y) {
         return (sVecs[x].count() < sVecs[y].count());
     }
   private:
-    SupVecs &sVecs;
+    const SupVecs &sVecs;
 };
 
 // reorders the circuit output, returns the configuration of each time-slot
 // remember to free the returned "slots" array afterwards
-static int* reordPO(SupVecs &sVecs, cuint nTimeFrame, cuint nCi, cuint nCo, cuint nPi, size_t &nPo, int *oPerm, TimeLogger *logger)
+static int* reordPO(const SupVecs &sVecs, cuint nTimeFrame, cuint nCi, cuint nCo, cuint nPi, size_t &nPo, int *oPerm, TimeLogger *logger)
 {
     // sort each CO by #1s in its support vector
     size_t *sIdx = new size_t[nCo];
@@ -122,10 +122,61 @@ static int* reordPO(SupVecs &sVecs, cuint nTimeFrame, cuint nCi, cuint nCo, cuin
     return slots;
 }
 
-//static size_t reordPO2()  // better pin sharing
+static double getScore(const vector<SupVecs> &sVecss, cuint t, cuint i, const double ratio = 0.9)
+{
+    if(sVecss[t][i].count() == 0) return 0.0;
+    double ret = 0.0;
+    for(size_t tt = 0; tt<t; ++tt)
+        ret += double((sVecss[t][i] & sVecss[tt][i]).count()) * pow(ratio, t-tt-1);
+    return ret;
+}
+
+// better pin sharing
+static void reordPO2(DdManager *dd, const SupVecs &sVecs, int *slots, cuint nTimeFrame, cuint nCi, cuint nCo, cuint nPi, cuint nPo, int *oPerm, TimeLogger *logger)
+{
+    vector<SupVecs> sVecss;  sVecss.reserve(nTimeFrame);
+    SupVec csv(nCi);
+    for(size_t t=0; t<nTimeFrame; ++t) {
+        sVecss.push_back(SupVecs(nPo, SupVec(nCi)));
+        for(size_t i=0; i<nPi; ++i)
+            csv[dd->invperm[t*nPi + i]] = 1;
+        assert(csv.count() == nPi*(t+1));
+
+        for(size_t i=0; i<nPo; ++i) if(slots[t*nPo + i] != -1) {
+            sVecss[t][i] = csv ^ sVecs[slots[t*nPo + i]];
+            assert(sVecss[t][i].count() <= nPi);
+        }
+    }
+
+    for(size_t t=1; t<nTimeFrame; ++t) for(size_t i=0; i<nPo; ++i) {
+        double maxScore = -1.0, score;
+        int maxIdx = -1;
+        for(size_t j=i; j<nPo; ++j) {
+            // compute score
+            score = getScore(sVecss, t, j);
+            if(score > maxScore) {
+                maxScore = score;
+                maxIdx = j;
+            }
+        }
+        assert((maxScore > 0.0) && (maxIdx > 0));
+
+        if(maxIdx != i) {
+            swap(slots[t*nPo + i], slots[t*nPo + maxIdx]);
+            swap(sVecss[t][i], sVecss[t][maxIdx]);
+        }
+    }
+
+    // update oPerm
+    for(size_t i=0; i<nPo*nTimeFrame; ++i) if(slots[i] != -1)
+        oPerm[slots[i]] = i;
+    assert(checkPerm(oPerm, nCo, nPo*nTimeFrame));
+
+    if(logger) logger->log("reord PO-2");
+}
 
 // places PIs at correct positions and reduces BDD size
-static void reordPI(DdManager *dd, SupVecs &sVecs, int *slots, cuint nTimeFrame, cuint nCi, cuint nPi, cuint nPo, int *iPerm, TimeLogger *logger)
+static void reordPI(DdManager *dd, const SupVecs &sVecs, int *slots, cuint nTimeFrame, cuint nCi, cuint nPo, int *iPerm, TimeLogger *logger)
 {
     size_t pos = 0;  // current PI position
     vector<size_t> sNums;  sNums.reserve(nTimeFrame);  // for reordering
@@ -134,8 +185,8 @@ static void reordPI(DdManager *dd, SupVecs &sVecs, int *slots, cuint nTimeFrame,
         size_t ppos = pos;
         SupVec sv2(nCi);  // supports of POs at current time-frame
         SupVec sv3(sv1);  // a copy of sv1
-        for(size_t i=0; i<nPo; ++i) if(slots[t*nTimeFrame + i] != -1)
-            sv2 |= sVecs[slots[t*nTimeFrame + i]];
+        for(size_t i=0; i<nPo; ++i) if(slots[t*nPo + i] != -1)
+            sv2 |= sVecs[slots[t*nPo + i]];
         
         sv1 |= sv2;  // upadate sv1
         sv3 ^= sv1;  // PIs needed at current time-frame
@@ -163,6 +214,8 @@ static void reordPI(DdManager *dd, SupVecs &sVecs, int *slots, cuint nTimeFrame,
     for(size_t i=0; i<nCi; ++i)
         iPerm[i] = dd->perm[i];
     assert(checkPerm(iPerm, nCi, nCi));
+
+    if(logger) logger->log("reord PI");
 }
 
 size_t reordIO(Abc_Ntk_t *pNtk, DdManager *dd, cuint nTimeFrame, int *iPerm, int *oPerm, TimeLogger *logger, const bool verbosity)
@@ -180,6 +233,8 @@ size_t reordIO(Abc_Ntk_t *pNtk, DdManager *dd, cuint nTimeFrame, int *iPerm, int
     size_t nPi = nCi / nTimeFrame;
 
     int *slots = reordPO(sVecs, nTimeFrame, nCi, nCo, nPi, nPo, oPerm, logger);
+    reordPI(dd, sVecs, slots, nTimeFrame, nCi, nPo, iPerm, logger);
+    reordPO2(dd, sVecs, slots, nTimeFrame, nCi, nCo, nPi, nPo, oPerm, logger);
 
 
     delete [] slots;
